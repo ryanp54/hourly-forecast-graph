bddf1d82d33617094e2889003c73667eaf059912
ryanp54 <ryanp54@yahoo.com>
2020-05-11 20:49:38 -0500

    Setup basic geolocation and forecast fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import getWeather from './nws-api';
+
 
 function App() {
+  const [weather, setWeather] = useState();
+  const [location, setLocation] = useGeolocation();
+
+  const onLocationChange = () => {
+    if (location) {
+      getWeather(location).then(
+        (forecast) => { debugger }
+      );
+    }
+  };
+
+  useEffect(onLocationChange, [location]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
 export default App;
+
+function useGeolocation() {
+  const [location, setLocation] = useState();
+  
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        ({coords}) => { setLocation(coords); },
+        () => { setLocation(null); }
+      )
+    } else {
+      setLocation(null);
+    }
+  }, []);
+
+  return [location, setLocation];
+}
---
+++ src/nws-api.js
@@ -0,0 +1,26 @@
+const nwsOptions = {
+  headers: {
+    'User-Agent': 'site:weather2019.appspot.com; contact-email:ryanp54@yahoo.com'
+  }
+};
+
+export default function getWeather({latitude, longitude}) {
+  return getGridpoints(latitude, longitude)
+    .then(getGridForecast);
+}
+
+function getGridpoints(latitude, longitude) {
+  return fetch(`https://api.weather.gov/points/${latitude},${longitude}`, nwsOptions)
+    .then(convertToJson);
+}
+
+function getGridForecast({cwa, gridX, gridY}) {
+  return fetch(
+    `https://api.weather.gov/gridpoints/${cwa}/${gridX},${gridY}`,
+    nwsOptions
+  ).then(convertToJson);
+}
+
+function convertToJson(resp) {
+  return resp.json().then((json) => json.properties);
+}
